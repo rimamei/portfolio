f7d64ee2087e6f6556910b85fb28b21634ec8576
Rima <rimameih@gmail.com>
2023-09-19 21:30:17 +0700

    feat: add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './page';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
     <Router>
       <Home />
+      <Analytics />
     </Router>
   );
 };
